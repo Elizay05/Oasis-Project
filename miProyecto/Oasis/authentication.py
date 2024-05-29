e69f7f9d0a6829ef359697cbd6dc0f0d758752fc
Sneyder Vergara Bolivar <sneyder_vergara@soy.sena.edu.co>
2024-05-29 11:17:41 -0500

    completo el de registro

--- miProyecto/Oasis/authentication.py
+++ miProyecto/Oasis/authentication.py
@@ -28,7 +28,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('rol', "ADMIN")
+        extra_fields.setdefault('rol', 1)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
--- miProyecto/Oasis/views.py
+++ miProyecto/Oasis/views.py
@@ -36,10 +36,17 @@ from .models import *
 
 # Create your views here.
 
+# ---------------------------------------------------------------------------------
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-	if created:
-		Token.objects.create(user=instance)
+    if created:
+        Token.objects.create(user=instance)
+
 
 
 def view_404(request, exception=None):
