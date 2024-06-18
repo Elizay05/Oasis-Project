19ef49404c67062a8840c499c64cd0e164f19c0a
Sayira Elittzi Quirama <sequirama@soy.sena.edu.co>
2024-06-17 19:12:26 -0500

    Ajustando detalles para pythonAnyWhere

--- miProyecto/miProyecto/settings.py
+++ miProyecto/miProyecto/settings.py
@@ -124,16 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/oasis/'
+MEDIA_URL = '/Oasis/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-"""
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-
-MEDIA_URL = 'tienda/'
-MEDIA_ROOT = BASE_DIR / 'media/'
-"""
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
